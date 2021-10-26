074e2d8aea1011cd799b34b58fcf05e1abc9f459
genyrosk <e.roskach@gmail.com>
2021-10-26 19:42:50 +0900

    style: add prettier and format

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,2 @@
-export { useDownloadAudio } from "./useDownloadAudio";
-export { useAudioMetadata } from "./useAudioMetadata"
+export { useDownloadAudio } from './useDownloadAudio'
+export { useAudioMetadata } from './useAudioMetadata'
--- src/hooks/useDownloadAudio.ts
+++ src/hooks/useDownloadAudio.ts
@@ -1,6 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
-import { box } from "tweetnacl";
-import { useState, useEffect } from "react";
+import { gql, useQuery } from '@apollo/client'
+import { box } from 'tweetnacl'
+import { useState, useEffect } from 'react'
 
 export const useDownloadAudio = ({
   audioId,
@@ -8,97 +8,95 @@ export const useDownloadAudio = ({
 }: AudioPlayerHookInput): AudioPlayerHookOutput => {
   const [response, setReponse] = useState<AudioPlayerHookOutput>({
     loading: true,
-  });
+  })
 
   const { loading, error, data } = useQuery(GET_REQUIRED_DOWNLOAD_INFO, {
     variables: { audioId },
-  });
+  })
 
   useEffect(() => {
     const downloadAudio = async (presignedUrl: string) => {
       try {
         const response = await fetch(presignedUrl, {
-          method: "GET",
-          credentials: "include",
-        });
-        console.log("response", response.status);
+          method: 'GET',
+          credentials: 'include',
+        })
+        console.log('response', response.status)
         const base64SymmetricKey =
-          data.getRequiredDownloadInfo.base64SymmetricKey;
-        console.log("base64SymmetricKey", base64SymmetricKey);
-        const symmetricKey = Buffer.from(base64SymmetricKey, "base64");
-        console.log("symmetricKey");
+          data.getRequiredDownloadInfo.base64SymmetricKey
+        console.log('base64SymmetricKey', base64SymmetricKey)
+        const symmetricKey = Buffer.from(base64SymmetricKey, 'base64')
+        console.log('symmetricKey')
         const encryptedAudioArrayBuffer = await response.arrayBuffer()
         const encryptedAudio = new Uint8Array(encryptedAudioArrayBuffer)
-        console.log("encryptedAudio");
-        const decryptedAudio = decrypt(symmetricKey, encryptedAudio);
-        console.log("decryptedAudio");
-        const blob = new Blob([decryptedAudio], { type: mimeType });
-        const audioUrl = URL.createObjectURL(blob);
-        console.log("audioUrl", audioUrl);
-        setReponse({ loading: false, audioSrc: audioUrl });
+        console.log('encryptedAudio')
+        const decryptedAudio = decrypt(symmetricKey, encryptedAudio)
+        console.log('decryptedAudio')
+        const blob = new Blob([decryptedAudio], { type: mimeType })
+        const audioUrl = URL.createObjectURL(blob)
+        console.log('audioUrl', audioUrl)
+        setReponse({ loading: false, audioSrc: audioUrl })
       } catch (e) {
-        console.error(e);
-        setReponse({ loading: false, error: JSON.stringify(e) });
+        console.error(e)
+        setReponse({ loading: false, error: JSON.stringify(e) })
       }
-    };
+    }
 
     if (
       loading === false &&
       error === undefined &&
       data?.getRequiredDownloadInfo?.presignedUrl !== undefined
     ) {
-      downloadAudio(data.getRequiredDownloadInfo.presignedUrl);
+      downloadAudio(data.getRequiredDownloadInfo.presignedUrl)
     }
-  }, [data, loading, error]);
+  }, [data, loading, error])
 
   if (loading) {
-    return { loading: true };
+    return { loading: true }
   }
   if (error) {
-    return { loading: false, error: error.message };
+    return { loading: false, error: error.message }
   }
 
-  return response;
-};
+  return response
+}
 
 export interface AudioPlayerHookInput {
-  audioId: string;
-  mimeType: string;
+  audioId: string
+  mimeType: string
 }
 
 interface AudioPlayerHookOutput {
-  audioSrc?: string;
-  htmlAudioElement?: HTMLAudioElement;
-  loading?: boolean;
-  error?: string;
+  audioSrc?: string
+  htmlAudioElement?: HTMLAudioElement
+  loading?: boolean
+  error?: string
 }
 
 const GET_REQUIRED_DOWNLOAD_INFO = gql`
   query getRequiredDownloadInfo($audioId: String!) {
-    getRequiredDownloadInfo(
-      audioId: $audioId
-    ) {
+    getRequiredDownloadInfo(audioId: $audioId) {
       base64SymmetricKey
       presignedUrl
     }
   }
-`;
+`
 
 export const decrypt = (
   secretOrSharedKey: Uint8Array,
-  messageWithNonce: Uint8Array
+  messageWithNonce: Uint8Array,
 ): Uint8Array => {
-  const nonce = messageWithNonce.slice(0, box.nonceLength);
+  const nonce = messageWithNonce.slice(0, box.nonceLength)
   const message = messageWithNonce.slice(
     box.nonceLength,
-    messageWithNonce.length
-  );
+    messageWithNonce.length,
+  )
 
-  const decrypted = box.open.after(message, nonce, secretOrSharedKey);
+  const decrypted = box.open.after(message, nonce, secretOrSharedKey)
 
   if (!decrypted) {
-    throw new Error("Could not decrypt message");
+    throw new Error('Could not decrypt message')
   }
 
-  return decrypted;
-};
+  return decrypted
+}
